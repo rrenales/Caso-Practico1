7810d3500a4fe60fac0fd127cf41acb557f873f7
EC2 Default User <ec2-user@ip-172-31-24-57.ec2.internal>
2021-05-14 21:19:24 +0000

    translate test

--- todos/translate.py
+++ todos/translate.py
@@ -1,7 +1,23 @@
 import boto3
+import logging
+def translate(event, context):
+    client = boto3.client('translate', region_name="us-eat-1")
+    
+    target_language = event['pathParameters']['target_lang']
+    text_id = event['pathParameters']['id']
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
 
-client = boto3.client('translate', region_name="us-eat-1")
-text = "Hola soc el ruben"
-result = client.translate_text(Text=text, SourceLanguageCode="auto",
-    TargetLanguageCode="en")
-print(result['TranslatedText'])
+    # fetch todo from the database
+    text = table.get_item(
+        Key={
+            'id': text_id
+        }
+    )
+    
+    
+    result = client.translate_text(Text=text, SourceLanguageCode="auto",
+        TargetLanguageCode=target_language)
+    print(result['TranslatedText'])
+      
+    return result
+    
