a73c548521385c5dfc862f63918581344efb94ee
Latra <personadelmonton@gmail.com>
2021-05-14 10:09:14 +0200

    Update translate.py

--- todos/translate.py
+++ todos/translate.py
@@ -1,16 +1,18 @@
 import boto3
 import logging
 def translate(event, context):
-    data = json.loads(event['body'])
-    if 'text' not in data:
-        logging.error("Validation Failed")
-        raise Exception("Couldn't create the todo item.")
-    client = boto3.client('translate', region_name="us-eat-1")
-    target_language = data['language']
-    if target_language == None:
-        logging.error("target language data missing")
-        raise Exception("Couldn't read the target language")
-    text = "Hola soc el ruben"
+    target_language = event['pathParameters']['target_lang']
+    text_id = event['pathParameters']['id']
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+
+    # fetch todo from the database
+    text = table.get_item(
+        Key={
+            'id': text_id
+        }
+    )
+    
+    
     result = client.translate_text(Text=text, SourceLanguageCode="auto",
         TargetLanguageCode=target_language)
     print(result['TranslatedText'])
