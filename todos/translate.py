cb6b2eb0bd07d48d2db5924c0bf25eb2f2fa87cd
EC2 Default User <ec2-user@ip-172-31-24-57.ec2.internal>
2021-05-14 07:11:16 +0000

    oks

--- todos/translate.py
+++ todos/translate.py
@@ -1,6 +1,6 @@
 import boto3
 
-client = boto3.client('p6bjkplwjh.execute-api.us-east-1.amazonaws.com/Review/todos/', region_name="us-eat-1")
+client = boto3.client('translate', region_name="us-eat-1")
 text = "Hola soc el ruben"
 result = client.translate_text(Text=text, SourceLanguageCode="auto",
     TargetLanguageCode="en")
