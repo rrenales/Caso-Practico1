7395af44b05528caf080b21a5342ced760add63e
Latra <personadelmonton@gmail.com>
2021-05-14 09:49:21 +0200

    Update translate.py

--- todos/translate.py
+++ todos/translate.py
@@ -1,7 +1,16 @@
 import boto3
-
-client = boto3.client('translate', region_name="us-eat-1")
-text = "Hola soc el ruben"
-result = client.translate_text(Text=text, SourceLanguageCode="auto",
-    TargetLanguageCode="en")
-print(result['TranslatedText'])
+import logging
+def translate(event, context):
+    data = json.loads(event['body'])
+    if 'text' not in data:
+        logging.error("Validation Failed")
+        raise Exception("Couldn't create the todo item.")
+    client = boto3.client('translate', region_name="us-eat-1")
+    target_language = data['language']
+    if target_language == None:
+        logging.error("target language data missing")
+        raise Exception("Couldn't read the target language")
+    text = "Hola soc el ruben"
+    result = client.translate_text(Text=text, SourceLanguageCode="auto",
+        TargetLanguageCode="en")
+    print(result['TranslatedText'])
